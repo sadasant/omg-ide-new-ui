a18334b0a19c636ea60e5cfbf465c64f5eabf166
Daniel Rodrí­guez <sadasant@gmail.com>
2014-07-21 20:02:16 -0430

    Left panel concept and basic css

---
+++ js/leftblock.js
@@ -0,0 +1,37 @@
+$(window).ready(function() {
+
+    var $block         = $("#left-block");
+    var $bar           = $("#left-bar");
+    var $panel         = $("#left-panel");
+    var $panel_content = $("#left-panel .content");
+    var $buttons       = $bar.find('[class*="entypo-"]');
+
+    $buttons.click(function() {
+        var $this = $(this);
+
+        if ($this.hasClass("active")) {
+            $buttons.removeClass("active");
+            closePanel();
+            return;
+        }
+
+        $buttons.removeClass("active");
+        $this.toggleClass("active");
+        openPanel();
+    });
+
+    function openPanel() {
+        $block.css({ borderRight: "1px solid #37a"});
+        $panel.animate({ width: 300 }, 300, function() {
+            $panel_content.show();
+        });
+    }
+
+    function closePanel() {
+        $panel_content.hide();
+        $panel.animate({ width: 0 }, 300, function() {
+            $block.css({ borderRight: "none"});
+        });
+    }
+
+});
