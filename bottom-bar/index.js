2a43331b2a9ee312980a06f123d72fe43a077746
Daniel Rodrí­guez <sadasant@gmail.com>
2014-07-29 15:16:34 -0430

    we don't need these files

--- bottom-bar/index.js
+++
@@ -1,7 +0,0 @@
-var connect = require('connect');
-var path = __dirname+'/';
-var port = process.env.PORT || 8080;
-connect.createServer(
-    connect.static(path)
-).listen(port);
-console.log("Serving "+path+" at: localhost:"+port);
